c11f2ff5b658c4a35b4c9378092890b5cbd6c4e4
caio86 <caioluiz86@gmail.com>
2025-11-20 22:13:00 -0300

    refactor: move functions createMediaCard, createMediaList to utils.js

--- script/script.js
+++ script/script.js
@@ -1,66 +1,8 @@
 import { listaConteudo } from "./api.js";
-import { capitalizeString } from "./utils.js";
+import { capitalizeString, createMediaList } from "./utils.js";
 
 const noContent = document.querySelector("#noContent");
 const container = document.querySelector(".container");
-
-function createMediaCard({
-  nome,
-  data_lancamento,
-  banner,
-  poster,
-  id,
-  vertical = false,
-}) {
-  const card = document.createElement("div");
-  card.className = `card card--${vertical ? "vertical" : "horizontal"}`;
-
-  card.innerHTML = `
-    <a href="./details.html?id=${id}" class="card__link">
-      <div class="card__img-wrapper">
-        <img
-          src="${vertical ? poster : banner}"
-          alt="${nome}"
-          class="card__img"
-          loading="lazy"
-          onerror="this.src = '/public/fallback-image.png'"
-        >
-        <div class="card__overlay">P</div>
-      </div>
-      <p class="card__name">${nome}</p>
-      <p class="card__year">${data_lancamento.split("-")[0]}</p>
-    </a>
-  `;
-
-  return card;
-}
-
-function createMediaList(headingName, media) {
-  // Create main wrapper
-  const listWrapper = document.createElement("div");
-  listWrapper.className = "list-horizontal-wrapper";
-
-  // Create heading
-  const heading = document.createElement("h2");
-  heading.className = "list-name";
-  heading.textContent = headingName;
-
-  // Create horizontal list
-  const listHorizontal = document.createElement("div");
-  listHorizontal.className = "list-horizontal";
-
-  // Create cards for each movie
-  const fragment = document.createDocumentFragment();
-  media.forEach((media) => {
-    fragment.appendChild(createMediaCard({ vertical: false, ...media }));
-  });
-
-  // Assemble final structure
-  listHorizontal.appendChild(fragment);
-  listWrapper.append(heading, listHorizontal);
-
-  return listWrapper;
-}
 
 const conteudo = await listaConteudo();
 let hasContent = false;
@@ -70,7 +12,11 @@ for (let key in conteudo) {
     continue;
   }
 
-  const list = createMediaList(capitalizeString(key), conteudo[key]);
+  const list = createMediaList(
+    capitalizeString(key),
+    conteudo[key],
+    key === "series",
+  );
   if (list) {
     container.appendChild(list);
     hasContent = true;
--- script/utils.js
+++ script/utils.js
@@ -5,3 +5,61 @@ export function capitalizeString(str) {
 
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export function createMediaCard({
+  nome,
+  data_lancamento,
+  banner,
+  poster,
+  id,
+  vertical = false,
+}) {
+  const card = document.createElement("div");
+  card.className = `card card--${vertical ? "vertical" : "horizontal"}`;
+
+  card.innerHTML = `
+    <a href="./details.html?id=${id}" class="card__link">
+      <div class="card__img-wrapper">
+        <img
+          src="${vertical ? poster : banner}"
+          alt="${nome}"
+          class="card__img"
+          loading="lazy"
+          onerror="this.src = '/public/fallback-image.png'"
+        >
+        <div class="card__overlay">P</div>
+      </div>
+      <p class="card__name">${nome}</p>
+      <p class="card__year">${data_lancamento.split("-")[0]}</p>
+    </a>
+  `;
+
+  return card;
+}
+
+export function createMediaList(headingName, media, vertical = false) {
+  // Create main wrapper
+  const listWrapper = document.createElement("div");
+  listWrapper.className = "list-horizontal-wrapper";
+
+  // Create heading
+  const heading = document.createElement("h2");
+  heading.className = "list-name";
+  heading.textContent = headingName;
+
+  // Create horizontal list
+  const listHorizontal = document.createElement("div");
+  listHorizontal.className = "list-horizontal";
+
+  // Create cards for each movie
+  const fragment = document.createDocumentFragment();
+  media.forEach((media) => {
+    fragment.appendChild(createMediaCard({ vertical, ...media }));
+  });
+
+  // Assemble final structure
+  listHorizontal.appendChild(fragment);
+  listWrapper.append(heading, listHorizontal);
+
+  return listWrapper;
+}
