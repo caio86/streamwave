23b95c52d8869b9e874de7c78e8c0bc9ccada472
caio86 <caioluiz86@gmail.com>
2026-01-23 10:46:19 -0300

    feat: refactor Filme class to FilmeModel with CRUD operations using Prisma

--- src/server/models/Filme.js
+++ src/server/models/Filme.js
@@ -1,5 +1,38 @@
-class Filme {
-  async read(where)
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+class FilmeModel {
+  async create(data) {
+    return prisma.filme.create({ data });
+  }
+
+  async findById(conteudoId) {
+    return prisma.filme.findUnique({
+      where: { conteudoId },
+      include: { conteudo: true },
+    });
+  }
+
+  async findAll(where = {}) {
+    return prisma.filme.findMany({
+      where,
+      include: { conteudo: true },
+    });
+  }
+
+  async update(conteudoId, data) {
+    return prisma.filme.update({
+      where: { conteudoId },
+      data,
+    });
+  }
+
+  async delete(conteudoId) {
+    return prisma.filme.delete({
+      where: { conteudoId },
+    });
+  }
 }
 
-export new FilmeModel()
+export default new FilmeModel();
