a1b587f8fe6275ea2aebdd85bfb78c8ce9ae9d72
caio86 <caioluiz86@gmail.com>
2026-01-23 00:31:19 -0300

    feat: update package.json
    - add nodemon
    - remove vite-express
    - remove json-server
    - add scripts for executing client and server

--- src/server/main.js
+++ src/server/main.js
@@ -1,43 +1,38 @@
-import "dotenv/config"
-import express from "express"
-import morgan from "morgan"
-import cors from "cors"
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import routes from "./routes/index.js";
 
-import ViteExpress from "vite-express"
+const PORT = process.env.PORT || 3000;
 
-import routes from "./routes"
+const app = express();
 
-const PORT = process.env.PORT || 3000
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-const app = express()
+app.use("/api", routes);
 
-app.use(express.json())
-app.use(cors())
-app.use(morgan("dev"))
+app.use((_req, res, _next) => {
+  res.status(404).json({
+    status: 404,
+    message: "404 - Not Found",
+  });
+});
 
-app.use("/api", routes)
+app.use((err, _req, res, _next) => {
+  console.error(err);
 
-app.use((req, res, next) => {
-	res.status(404).json({
-		status: 404,
-		message: "404 - Not Found"
-	})
-})
+  const statusCode = err.status || 500;
+  const message = err.message || "Internal Server Error";
 
-app.use((err, req, res, next) => {
-	console.error(err)
+  res.status(statusCode).json({
+    status: statusCode,
+    message: message,
+  });
+});
 
-	const statusCode = err.status || 500
-	const message = err.message || "Internal Server Error"
-
-	res.status(statusCode).json({
-		status: statusCode,
-		message: message,
-	})
-})
-
-
-const server = app.listen(PORT, () => {
-	console.log("Server is running...")
-})
-ViteExpress.bind(app, server);
+app.listen(PORT, () => {
+  console.log("Server is running...");
+});
