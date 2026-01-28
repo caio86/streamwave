67cb00edbaaa3501e785b169786ce841bfa37b54
caio86 <caioluiz86@gmail.com>
2026-01-27 21:44:15 -0300

    chore: apply prettier format

--- src/server/controllers/filme.controller.js
+++ src/server/controllers/filme.controller.js
@@ -1,59 +1,59 @@
-import FilmeService from "../services/filme.service.js"
+import FilmeService from "../services/filme.service.js";
 
 class FilmeController {
   async index(req, res, next) {
     try {
-      const filmes = await FilmeService.getAll()
-      res.json(filmes)
+      const filmes = await FilmeService.getAll();
+      res.json(filmes);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   async getByID(req, res, next) {
     try {
-      const { conteudoId } = req.params
+      const { conteudoId } = req.params;
 
-      const filme = await FilmeService.getById(conteudoId)
+      const filme = await FilmeService.getById(conteudoId);
 
-      res.json(filme)
+      res.json(filme);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   async create(req, res, next) {
     try {
-      const createdFilme = await FilmeService.create(req.body)
-      res.status(201).json(createdFilme)
+      const createdFilme = await FilmeService.create(req.body);
+      res.status(201).json(createdFilme);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   async update(req, res, next) {
     try {
-      const { conteudoId } = req.params
+      const { conteudoId } = req.params;
 
-      const updatedFilme = await FilmeService.update(conteudoId, req.body)
+      const updatedFilme = await FilmeService.update(conteudoId, req.body);
 
-      res.json(updatedFilme)
+      res.json(updatedFilme);
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 
   async delete(req, res, next) {
     try {
-      const { conteudoId } = req.params
+      const { conteudoId } = req.params;
 
-      await FilmeService.delete(conteudoId)
+      await FilmeService.delete(conteudoId);
 
-      res.status(204).send()
+      res.status(204).send();
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
 }
 
-export default new FilmeController()
+export default new FilmeController();
