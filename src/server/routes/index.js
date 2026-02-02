d19bad73f5f3ad64de3db893c616c0ccd1fbed92
felipe <silvadesenvolvedor@gmail.com>
2026-02-02 09:33:03 -0300

    Rotas criadas, series, temporas e episodios .

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import filmesRoutes from "./filmes.routes.js";
 import seriesRoutes from "./series.routes.js";
+import temporadaRoutes from "./temporadas.routes.js";
+import episodiosRoutes from "./episodios.routes.js";
 
 const routes = Router();
 
 routes.use("/filmes", filmesRoutes);
 routes.use("/series", seriesRoutes);
+routes.use("/series/:conteudoId/temporadas", temporadaRoutes);
+routes.use("/temporadas/:temporadaId/episodios", episodiosRoutes);
+
 
 export default routes;
--- src/server/routes/series.routes.js
+++ src/server/routes/series.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import SerieController from "../controllers/serie.controller.js";
+import temporadaRoutes from "./temporada.routes.js";
 
 const routes = Router();
+
+routes.get("/", SerieController.index);
+routes.get("/:conteudoId", SerieController.getByID);
+routes.post("/", SerieController.create);
+routes.put("/:conteudoId", SerieController.update);
+routes.delete("/:conteudoId", SerieController.delete);
+
+// Rotas aninhadas de temporadas
+routes.use("/:conteudoId/temporadas", temporadaRoutes);
 
 export default routes;
