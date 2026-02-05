04bed6fdbedd3a6a9bfd2f947c3584580a88a001
caio86 <caioluiz86@gmail.com>
2026-02-05 00:12:30 -0300

    docs: create episodio, serie and temporadas routes documentation

--- src/server/routes/series.routes.js
+++ src/server/routes/series.routes.js
@@ -2,6 +2,105 @@ import { Router } from "express";
 import SerieController from "../controllers/serie.controller.js";
 import temporadaRoutes from "./temporadas.routes.js";
 
+/**
+ * @swagger
+ * /series:
+ *   get:
+ *     summary: Listar séries
+ *     description: Retorna uma lista de séries disponíveis.
+ *     tags:
+ *       - Series
+ *     responses:
+ *       200:
+ *         description: Lista de séries retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Serie'
+ *
+ *   post:
+ *     summary: Criar série
+ *     description: Cria um novo conteúdo do tipo série.
+ *     tags:
+ *       - Series
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Serie'
+ *     responses:
+ *       201:
+ *         description: Série criada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Serie'
+ *
+ * /series/{conteudoId}:
+ *   get:
+ *     summary: Obter série
+ *     description: Retorna uma série pelo conteudoId.
+ *     tags:
+ *       - Series
+ *     parameters:
+ *       - in: path
+ *         name: conteudoId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID (UUID) do conteúdo da série
+ *     responses:
+ *       200:
+ *         description: Série retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Serie'
+ *
+ *   put:
+ *     summary: Atualizar série
+ *     description: Atualiza os dados da série indicada por conteudoId.
+ *     tags:
+ *       - Series
+ *     parameters:
+ *       - in: path
+ *         name: conteudoId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Serie'
+ *     responses:
+ *       200:
+ *         description: Série atualizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Serie'
+ *
+ *   delete:
+ *     summary: Excluir série
+ *     description: Remove a série e seus relacionamentos (cascata).
+ *     tags:
+ *       - Series
+ *     parameters:
+ *       - in: path
+ *         name: conteudoId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Série excluída com sucesso
+ */
+
 const routes = Router();
 
 routes.get("/", SerieController.index);
--- src/server/routes/temporadas.routes.js
+++ src/server/routes/temporadas.routes.js
@@ -1,5 +1,131 @@
+/**
+ * @swagger
+ * /series/{serieId}/temporadas:
+ *   get:
+ *     summary: Listar temporadas de uma série
+ *     description: Retorna as temporadas pertencentes a uma série.
+ *     tags:
+ *       - Temporadas
+ *     parameters:
+ *       - in: path
+ *         name: serieId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID (UUID) do conteúdo da série
+ *     responses:
+ *       200:
+ *         description: Lista de temporadas retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Temporada'
+ *
+ *   post:
+ *     summary: Criar temporada
+ *     description: Cria uma nova temporada vinculada à série informada.
+ *     tags:
+ *       - Temporadas
+ *     parameters:
+ *       - in: path
+ *         name: serieId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Temporada'
+ *     responses:
+ *       201:
+ *         description: Temporada criada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Temporada'
+ *
+ * /series/{serieId}/temporadas/{temporadaId}:
+ *   get:
+ *     summary: Obter temporada
+ *     description: Retorna os dados de uma temporada específica.
+ *     tags:
+ *       - Temporadas
+ *     parameters:
+ *       - in: path
+ *         name: serieId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: temporadaId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Temporada retornada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Temporada'
+ *
+ *   put:
+ *     summary: Atualizar temporada
+ *     description: Atualiza os dados de uma temporada existente.
+ *     tags:
+ *       - Temporadas
+ *     parameters:
+ *       - in: path
+ *         name: serieId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: temporadaId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Temporada'
+ *     responses:
+ *       200:
+ *         description: Temporada atualizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Temporada'
+ *
+ *   delete:
+ *     summary: Excluir temporada
+ *     description: Remove uma temporada e seus episódios (cascata).
+ *     tags:
+ *       - Temporadas
+ *     parameters:
+ *       - in: path
+ *         name: serieId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: path
+ *         name: temporadaId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Temporada excluída com sucesso
+ */
 import { Router } from "express";
 import TemporadaController from "../controllers/temporada.controller.js";
+import episodiosRoutes from "./episodios.routes.js";
 
 const routes = Router({ mergeParams: true });
 
@@ -8,5 +134,8 @@ routes.get("/:temporadaId", TemporadaController.getByID);
 routes.post("/", TemporadaController.create);
 routes.put("/:temporadaId", TemporadaController.update);
 routes.delete("/:temporadaId", TemporadaController.delete);
+
+// Roteamento aninhado para episódios
+routes.use("/:temporadaId/episodios", episodiosRoutes);
 
 export default routes;
